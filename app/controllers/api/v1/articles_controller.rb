c20dce923e8dde8e26b3527de33e974f134f0ae1
Matheusafonsouza <matheusafonsouza@gmail.com>
2022-12-20 00:53:12 -0300

    feat: add rest of the article crud

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -15,7 +15,55 @@ module Api
                 render json: {
                     status: "SUCCESS",
                     article: article
-                }
+                }, status: :ok
+            end
+
+            def create
+                article = Article.new(article_params)
+
+                if article.save
+                    render json: {
+                        status: "SUCESS",
+                        article: article
+                    }, status: :ok
+                else
+                    render json: {
+                        status: "ERROR",
+                        message: "Article not saved.",
+                        errors: article.erros
+                    }, status: :unprocessable_entity
+                end
+            end
+
+            def destroy
+                article = Article.find(params[:id])
+                article.destroy
+                render json: {
+                    status: "SUCESS",
+                    message: "Article deleted."
+                }, status: :ok
+            end
+
+            def update
+                article = Article.find(params[:id])
+
+                if article.update_attributes(article_params)
+                    render json: {
+                        status: "SUCESS",
+                        article: article
+                    }, status: :ok
+                else
+                    render json: {
+                        status: "ERROR",
+                        message: "Cannot update article.",
+                        errors: article.erros
+                    }, status: :unprocessable_entity
+                end
+            end
+
+            private
+            def article_params
+                params.permit(:title, :body)
             end
 
         end
