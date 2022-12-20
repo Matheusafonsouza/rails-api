c0e4ab2edca08115c09f23ad0075f9733a36d4aa
Matheusafonsouza <matheusafonsouza@gmail.com>
2022-12-20 00:48:01 -0300

    feat: add show article

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -3,11 +3,19 @@ module Api
         class ArticlesController < ApplicationController
 
             def index
-                articles = Article.order("created_at DESC");
+                articles = Article.order("created_at DESC")
                 render json: {
                     status: "SUCCESS",
                     artciles: articles
                 }, status: :ok
+            end
+
+            def show
+                article = Article.find(params[:id])
+                render json: {
+                    status: "SUCCESS",
+                    article: article
+                }
             end
 
         end
